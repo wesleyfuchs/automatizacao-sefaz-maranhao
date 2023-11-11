efa6b0222504632dce8a07adf96115f88cfaa8e1
Wesley Fuchs <fuchswesley@hotmail.com>
2023-11-11 08:28:17 -0300

    ler imagem da web

--- teste.py
+++ teste.py
@@ -1,40 +1,38 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 import pytesseract
 from io import BytesIO
 
-# # Caminho para o executável do Tesseract (altere conforme necessário)
+# Caminho para o executável do Tesseract (altere conforme necessário)
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# URL da página web
-url = 'sua_url_aqui'
+# URL da página
+url_pagina = 'https://quadlayers.com/add-captcha-to-woocommerce-login/'
 
-# Inicializar o driver do Selenium (certifique-se de ter o WebDriver adequado para o seu navegador)
-driver = webdriver.Chrome()
-driver.get(url)
+# Função para obter a URL da imagem a partir do HTML da página
+def obter_url_imagem(url_pagina):
+    response = requests.get(url_pagina)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# Obter o código HTML da página
-html = driver.page_source
+    # Substitua 'class_da_imagem' pela classe real da imagem
+    imagem = soup.find('img', {'class': 'aligncenter'})
+    url_imagem = imagem['src']
 
-# Fechar o navegador
-driver.quit()
+    return url_imagem
 
-# Analisar o HTML com BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
+# Função para realizar OCR em uma imagem a partir da URL
+def ocr_em_imagem_url(url):
+    response = requests.get(url)
+    imagem = Image.open(BytesIO(response.content))
+    texto = pytesseract.image_to_string(imagem)
+    return texto
 
-# Encontrar a tag img com o id "form1:captcha"
-img_tag = soup.find('img', {'id': 'form1:captcha'})
+# Obtendo a URL da imagem a partir do HTML da página
+url_imagem = obter_url_imagem(url_pagina)
 
-# Obter o valor do atributo src
-img_src = img_tag['src']
-
-# Ler a imagem diretamente do atributo src usando BytesIO
-img_data = BytesIO(driver.get(img_src).content)
-imagem = Image.open(img_data)
-
-# Extrair texto da imagem
-texto_extraido = pytesseract.image_to_string(imagem)
+# Exemplo de uso
+texto_extraido = ocr_em_imagem_url(url_imagem)
 
 # Exibindo o texto extraído
 print("Texto extraído:")
