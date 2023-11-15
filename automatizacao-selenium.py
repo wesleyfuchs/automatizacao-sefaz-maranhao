03132abc168ca209e4dd7aa641747b8ced1d7b47
uesleyfucks <fuchswesley@hotmail.com>
2023-11-15 19:20:30 -0300

    criado funcao ocr

--- automatizacao-selenium.py
+++ automatizacao-selenium.py
@@ -10,8 +10,34 @@ from PIL import Image
 import pytesseract
 import time
 
+# Caminho do Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+def ocr_imagem(elemento_imagem):
+    # Obter a localização e as dimensões da imagem
+    localizacao = elemento_imagem.location
+    tamanho = elemento_imagem.size
+
+    # Capturar a área da tela correspondente à imagem
+    driver.save_screenshot("screenshot.png")
+    imagem_screenshot = Image.open("screenshot.png")
+    area_imagem = (
+        localizacao['x'], localizacao['y'],
+        localizacao['x'] + tamanho['width'], localizacao['y'] + tamanho['height']
+    )
+    imagem_cortada = imagem_screenshot.crop(area_imagem)
+    imagem_cortada.save("imagem_cortada.png")
+
+    # Usar o PyTesseract para extrair texto da imagem
+    texto_extraido = pytesseract.image_to_string(Image.open("imagem_cortada.png"))
+    # texto_extraido = texto_extraido.strip()
+    # Remover todos os espaços possíveis (espaços em branco, tabulações, quebras de linha)
+    texto_extraido = texto_extraido.replace(" ", "").replace("\n", "").replace("\t", "")
+
+    # Imprimir o texto extraído
+    print(texto_extraido)
+    
+    return texto_extraido
 
 # Inicializar o driver do Selenium (certifique-se de ter o WebDriver correspondente ao seu navegador instalado)
 # Configurar o webdriver
@@ -32,8 +58,6 @@ driver.implicitly_wait(10)
 iframe = driver.find_element(By.NAME, 'mainFrame')
 driver.switch_to.frame(iframe)
 
-
-
 # Encontrar o elemento IE Empresa
 input_empresa = driver.find_element(By.NAME, 'form1:j_id11')
 input_empresa.send_keys("123235669") 
@@ -49,15 +73,7 @@ time.sleep(1)
 
 # Encontrar o elemento do calendario pop up (data inicial)
 element = driver.find_element(By.ID, 'form1:dtIniPopupButton')
-
-# # Obter as coordenadas do elemento
-# element_location = element.location
-# element_size = element.size
-
-# # Exibir as coordenadas
-# print("Coordenadas X:", element_location['x'])
-# print("Coordenadas Y:", element_location['y'])
-
+# Executa as ações
 element.click()
 time.sleep(1)
 botao_calendario = driver.find_element(By.ID, 'form1:dtIniDayCell2')
@@ -65,7 +81,7 @@ botao_calendario.click()
 
 # Encontrar o elemento do calendario pop up (data final)
 element = driver.find_element(By.ID, 'form1:dtFinPopupButton')
-
+# Executa as ações
 element.click()
 time.sleep(1)
 botao_calendario = driver.find_element(By.ID, 'form1:dtFinDayCell15')
@@ -74,28 +90,7 @@ botao_calendario.click()
 # Localizar o elemento da imagem pelo ID 
 elemento_imagem = driver.find_element(By.ID, "form1:captcha")
 
-# Obter a localização e as dimensões da imagem
-localizacao = elemento_imagem.location
-tamanho = elemento_imagem.size
-
-# Capturar a área da tela correspondente à imagem
-driver.save_screenshot("screenshot.png")
-imagem_screenshot = Image.open("screenshot.png")
-area_imagem = (
-    localizacao['x'], localizacao['y'],
-    localizacao['x'] + tamanho['width'], localizacao['y'] + tamanho['height']
-)
-imagem_cortada = imagem_screenshot.crop(area_imagem)
-imagem_cortada.save("imagem_cortada.png")
-
-# Usar o PyTesseract para extrair texto da imagem
-texto_extraido = pytesseract.image_to_string(Image.open("imagem_cortada.png"))
-# texto_extraido = texto_extraido.strip()
-# Remover todos os espaços possíveis (espaços em branco, tabulações, quebras de linha)
-texto_extraido = texto_extraido.replace(" ", "").replace("\n", "").replace("\t", "")
-
-# Imprimir o texto extraído
-print(texto_extraido)
+texto_extraido = ocr_imagem(elemento_imagem)
 
 # Encontrar o campo para inserir o Captcha
 input_captcha = driver.find_element(By.NAME, 'form1:j_id35')
@@ -108,13 +103,54 @@ time.sleep(2)
 # Encontrar o elemento Baixar XML
 element_baixar_xml = driver.find_element(By.NAME, 'form1:j_id41')
 element_baixar_xml.click()
-time.sleep(3)
+# time.sleep(3)
 
+# Esperar até 10 segundos para ver se o download inicia ou uma mensagem é exibida
+try:
+    # Esperar pelo início do download
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ui-messages-warn-detail')))
+    
+    # Verificar se é uma mensagem de erro de captcha
+    try:
+        captcha_error_message = driver.find_element(By.XPATH, "//span[contains(@class, 'ui-messages-warn-detail') and text()='Código da imagem está inválido.']").text
+        if captcha_error_message:
+            print(f"Erro no captcha: {captcha_error_message}")
+            # Pode tentar novamente aqui antes de sair ou lançar uma exceção
+            # Localizar o elemento da imagem pelo ID 
+            elemento_imagem = driver.find_element(By.ID, "form1:captcha")
+            texto_extraido = ocr_imagem(elemento_imagem)
+            time.sleep(2)
+            # Encontrar o campo para inserir o Captcha
+            input_captcha = driver.find_element(By.NAME, 'form1:j_id35')
+            # Limpar o conteúdo do campo
+            input_captcha.clear()
+            input_captcha.send_keys(texto_extraido) 
+            time.sleep(2)
+            # Encontrar o elemento Baixar XML
+            element_baixar_xml = driver.find_element(By.NAME, 'form1:j_id41')
+            element_baixar_xml.click()
+    except NoSuchElementException:
+        # Se o bloco 'try' foi bem-sucedido, continue aqui para verificar outros elementos
+        pass
+    # Verificar se é uma mensagem de ausência de download
+    try:
+        no_download_message = driver.find_element(By.XPATH, "//span[contains(@class, 'ui-messages-warn-detail') and text()='A consulta foi realizada com sucesso porém não foram encontradas notas.']").text
+        if no_download_message:
+            print(f"Nada para baixar: {no_download_message}")
+            # Pode sair do loop ou fazer outra ação apropriada
+    except NoSuchElementException:
+        pass
+        # Se não houver mensagens relevantes, continue no loop ou faça outra ação apropriada
+except TimeoutException:
+    # Se não houver download iniciado, e não há mensagens de erro ou de ausência de download
+    # presume-se que o download foi iniciado corretamente
+    print("Download iniciado com sucesso!")
 
-# time.sleep(5)
+time.sleep(2)
 # Fechar o navegador
 driver.quit()
 
 
 # <span class="ui-messages-warn-detail">A consulta foi realizada com sucesso porém não foram encontradas notas.</span>
 # <span class="ui-messages-warn-detail">Código da imagem está inválido.</span>
+# <span class="ui-messages-warn-detail">Todos os campos com (*) devem ser informados.</span>
