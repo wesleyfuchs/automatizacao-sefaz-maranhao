d269698222a979ed3f86de4501137548823b22fb
uesleyfucks <fuchswesley@hotmail.com>
2023-11-26 11:38:34 -0300

    +tools e setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": [],
+    "include_files": ["tools/"],
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  # Use "Win32GUI" se você estiver criando uma GUI
+
+setup(
+    name="auto-sefaz-ma",
+    version="1.0",
+    description="Automatização de download de NF-e e NFC-e no site sefaz.ma.gov.br",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("auto-sefaz-ma.py", base=base)],
+)
